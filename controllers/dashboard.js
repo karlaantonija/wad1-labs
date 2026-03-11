0c9aa3328e21fe859dc7145a3c95331dba3910cc
[karlaantonija] <karlaantonija9@gmail.com>
2026-03-11 15:42:19 +0000

    Completed Delete Functions Lab

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -28,6 +28,14 @@ const dashboard = {
     playlistStore.addPlaylist(newPlayList);
     response.redirect('/dashboard');
   },
+  
+  deletePlaylist(request, response) {
+    const playlistId = request.params.id;
+    logger.debug(`Deleting Playlist ${playlistId}`);
+    playlistStore.removePlaylist(playlistId);
+    response.redirect("/dashboard");
+  },
+
 
 };
 
