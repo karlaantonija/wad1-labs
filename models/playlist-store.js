f687f8319cdfe1347f4692b0b470a1ae6615396e
[karlaantonija] <karlaantonija9@gmail.com>
2026-03-18 16:10:38 +0000

    helpers lab

--- models/playlist-store.js
+++ models/playlist-store.js
@@ -34,6 +34,9 @@ const playlistStore = {
     this.store.removeCollection(this.collection, playlist);
   },
 
+  editSong(id, songId, updatedSong) {
+    this.store.editItem(this.collection, id, songId, this.array, updatedSong);
+  },
 
 
 
