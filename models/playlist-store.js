0e2de2fa477dfb817506751cbac72363ca99b490
[karlaantonija] <karlaantonija9@gmail.com>
2026-03-25 16:15:41 +0000

    search

--- models/playlist-store.js
+++ models/playlist-store.js
@@ -38,6 +38,12 @@ const playlistStore = {
     this.store.editItem(this.collection, id, songId, this.array, updatedSong);
   },
 
+  searchPlaylist(search) {
+    return this.store.findBy(
+      this.collection,
+      (playlist => playlist.title.toLowerCase().includes(search.toLowerCase())))
+  }
+
 
 
 };
